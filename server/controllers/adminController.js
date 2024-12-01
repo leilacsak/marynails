bfdc4a704781ce5b102ec5ebcafd29a92d4b5d9a
leilacsak <leilacsak@gmail.com>
2024-12-01 22:37:44 +0000

    kommentek kiszedve

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -5,23 +5,6 @@ const pool = require('../db');
 
 // Admin bejelentkezés
 const adminLogin = async (req, res) => {
-  // const { email, password } = req.body;
-  //
-  // if (!email || !password) {
-  //   return res.status(400).json({ message: 'Email and password are required!' });
-  // }
-  //
-  // try {
-  //   const admin = await getAdminByEmail(email);
-  //   if (!admin) {
-  //     return res.status(400).json({ message: 'Invalid email or password.' });
-  //   }
-  //
-  //   const isValidPassword = await bcrypt.compare(password, admin.passwordhash);
-  //   if (!isValidPassword) {
-  //     return res.status(400).json({ message: 'Invalid email or password.' });
-  //   }
-
     const token = jwt.sign(
       { adminId: 1, role: 'admin' },
       process.env.JWT_SECRET,
@@ -37,10 +20,6 @@ const adminLogin = async (req, res) => {
         email: 'mail',
       },
     });
-  // } catch (error) {
-  //   console.error('Admin bejelentkezési hiba:', error.message);
-  //   res.status(500).json({ message: 'Error signing in.' });
-  // }
 };
 
 
